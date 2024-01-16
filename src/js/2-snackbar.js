713476b0e46172e624b2ce9d52a506c11deada02
pxlmady <yoplufre@gmail.com>
2024-01-16 20:03:26 +0200

    08

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,20 +1,29 @@
+// Імпорт бібліотеки для відображення повідомлень
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+// Встановлення обробників подій для форми при відправці
 document.querySelector('.form').addEventListener('submit', function (event) {
+    // Запобігання відправці форми за замовчуванням
   event.preventDefault();
 
+     // Отримання значень полів вводу затримки і стану
   const delayInput = document.querySelector('input[name="delay"]');
   const stateInput = document.querySelector('input[name="state"]:checked');
 
-  if (!delayInput || !stateInput) {
+     // Перевірка наявності обох полів вводу
+    if (!delayInput || !stateInput) {
+      // Виведення повідомлення про заповнення всіх полів
     console.error('Please fill in all fields.');
     return;
   }
 
+    // Парсинг значення затримки до цілого числа
   const delay = parseInt(delayInput.value, 10);
 
-  const promise = new Promise((resolve, reject) => {
+    // Створення об'єкта Promise
+    const promise = new Promise((resolve, reject) => {
+      // Встановлення затримки перед вирішенням або відхиленням обіцянки
     setTimeout(() => {
       if (stateInput.value === 'fulfilled') {
         resolve(delay);
@@ -24,8 +33,10 @@ document.querySelector('.form').addEventListener('submit', function (event) {
     }, delay);
   });
 
+    // Обробка виконання або відхилення обіцянки
   promise.then(
-    (value) => {
+      (value) => {
+          // Виведення повідомлення про успішне виконання обіцянки
       iziToast.success({
         title: '',
         message: `Fulfilled promise in ${value}ms! ✅`,
@@ -34,6 +45,7 @@ document.querySelector('.form').addEventListener('submit', function (event) {
       });
     },
     (reason) => {
+        // Виведення повідомлення про відхилення обіцянки
       iziToast.error({
         title: '',
         message: `Rejected promise in ${reason}ms! ❌`,
