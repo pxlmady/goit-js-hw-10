0bd02c44cfff40cf89af609d1b16896d6eeb315c
pxlmady <yoplufre@gmail.com>
2024-01-16 19:19:26 +0200

    02

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stateInput = document.querySelector('input[name="state"]:checked');
+
+  if (!delayInput || !stateInput) {
+    console.error('Please fill in all fields.');
+    return;
+  }
+
+  const delay = parseInt(delayInput.value, 10);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (stateInput.value === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise.then(
+    (value) => {
+      iziToast.success({
+        title: '',
+        message: `Fulfilled promise in ${value}ms ✅`,
+          position: 'topRight',
+        icon: '',
+      });
+    },
+    (reason) => {
+      iziToast.error({
+        title: '',
+        message: `Rejected promise in ${reason}ms ❌`,
+          position: 'topRight',
+        icon: '',
+      });
+    }
+  );
+});
